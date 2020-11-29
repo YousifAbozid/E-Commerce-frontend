9b1d952a6d6515838fb01d6d15cb6b94dc7ec5a8
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-29 06:21:01 +0200

    initialize the frontend with redux and added store with no reducers yet

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({
+
+})
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
+
+export default store
