f812d03936a7692453d4bce0ea62870f3ec6681c
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-29 14:32:47 +0200

    use action creator to fetch the data and display it in the component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import { getProducts } from '../api'
+import { productsList } from '../actions/product'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    // below this is destructuring for productList and I can put it in the line above but I let it here for explanation and to simplify
+    const { products, loading, errors } = productList
 
     useEffect(() => {
-        getProducts()
-            .then((response) => setProducts(response.data))
-            .catch((error) => console.log(error))
-    }, [])
+        dispatch(productsList())
+    }, [dispatch])
 
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
+            {loading
+            ? <h2>Loading...</h2>
+            : errors.length >= 1
+            ? <h3>error...</h3>
+            : <Row>
                 {products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+            </Row>}
         </div>
     )
 }
